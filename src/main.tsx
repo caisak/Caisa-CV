fb31c49f01ae90c1e71c08c27ad416b7206b8e28
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-04-21 21:19:53 +0200

    set up routing and create pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { AboutMePage } from './pages/AboutMePage'
+import { EducationPage } from './pages/EducationPage'
+import { ExperiencePage } from './pages/ExperiencePage'
+import { LeadershipPage } from './pages/LeadershipPage'
+import { NotFound } from './pages/NotFound'
+import { TechnicalSkillsPage } from './pages/TechnicalSkillsPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* Outlet start? */}
+      <Route index element={<AboutMePage />}></Route>
+      <Route path="education" element={<EducationPage />} />
+      <Route path="technicalskills" element={<TechnicalSkillsPage />} />
+      <Route path="experience" element={<ExperiencePage />} />
+      <Route path="leadership" element={<LeadershipPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 )
