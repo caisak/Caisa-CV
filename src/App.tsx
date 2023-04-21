51fe725df8b1d09b79a3709eb09b50eae04c3111
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-04-21 22:09:36 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import { HeaderResponsive } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const links = [
+    { link: '/', label: 'About Me' },
+    { link: '/education', label: 'Education' },
+    { link: '/technicalskills', label: 'Technical Skills' },
+    { link: '/experience', label: 'Experience' },
+    { link: '/leadership', label: 'Leadership' },
+  ]
+
   return (
     <div>
-      <h1>hello</h1>
+      <HeaderResponsive links={links} />
     </div>
   )
 }
