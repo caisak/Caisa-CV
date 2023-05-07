a5c7a332da207de5c83aab367a47e008faee7b88
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-07 21:29:37 +0200

    style on about me

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,66 @@
+import { Button, createStyles, Paper, rem, Title } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    height: rem(200),
+    width: '20rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+
+  title: {
+    fontFamily: `Greycliff CF ${theme.fontFamily}`,
+    fontWeight: 900,
+    color: theme.white,
+    lineHeight: 1.2,
+    fontSize: rem(32),
+    marginTop: theme.spacing.xs,
+  },
+
+  category: {
+    color: theme.white,
+    opacity: 0.7,
+    fontWeight: 700,
+    textTransform: 'uppercase',
+  },
+}));
+
+interface ArticleCardImageProps {
+  image: string;
+  title: string;
+  link: string;
+  website: string;
+}
+
+export function ArticleCardImage({ image, title, link, website }: ArticleCardImageProps) {
+  const { classes } = useStyles();
+
+  return (
+    <Paper
+      shadow="md"
+      p="xl"
+      radius="md"
+      sx={{ backgroundImage: `url(${image})` }}
+      className={classes.card}
+    >
+      <div>
+        <Title order={3} className={classes.title}>
+          {title}
+        </Title>
+      </div>
+      <Button variant="white" 
+      color="dark"
+      component="a"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {website}
+      </Button>
+    </Paper>
+  );
+}
--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,20 +1,48 @@
+import { Flex } from "@mantine/core";
+import { ArticleCardImage } from "../components/ArticleCard";
 import { EducationTimeline } from "../components/EducationTimeline";
 import { ExperienceTimeline } from "../components/ExperienceTimeline";
 import { ProgressCard } from "../components/TechSkills";
 
 export function AboutMePage() {
-    return(
+  return (
     <div>
-        <h1>About Me</h1>
-        <ProgressCard />
-            <h2>Libraries</h2>
-            <h3>React, Svelte</h3>
-            <h2>Other</h2>
-            <h3>Styled components, chakra ui, mantine ui, bootstrap</h3>
-        <h2>Education</h2>
-            <EducationTimeline />
-            <h2 align="right">Experience</h2>
-            <ExperienceTimeline />
+      <h1>About Me</h1>
+      <ProgressCard />
+      <h2>Libraries</h2>
+      <h3>React, Svelte</h3>
+      <h2>Other</h2>
+      <h3>Styled components, chakra ui, mantine ui, bootstrap</h3>
+      <div style={{ marginBottom: "1rem", marginTop: "1rem",  }}>
+        <h2>Selected Projects</h2>
+        <h2 style={{ textAlign: "right" }}>Go to Portfolio</h2>
+      </div>
+      <Flex justify="center" gap="xl">
+        <ArticleCardImage
+          image="/images/bobablissp.png"
+          title="Boba Bliss"
+          link="https://bobabliss.netlify.app/"
+          website="Go to Boba Bliss"
+        />
+
+        <ArticleCardImage
+          image="/images/vegokassenp.png"
+          title="Vegokassen"
+          link="https://vegokassen.netlify.app/"
+          website="Go to Vegokassen"
+        />
+
+        <ArticleCardImage
+          image="/images/moonmissionp.png"
+          title="Moon Mission"
+          link="https://lysmac.github.io/moon-mission/"
+          website="Go to Moon Mission"
+        />
+      </Flex>
+      <h2 style={{ marginBottom: "1rem", marginTop: "1rem",  }}>Education</h2>
+      <EducationTimeline />
+      <h2 style={{ textAlign: "right", marginBottom: "1rem", marginTop: "1rem",  }}>Experience</h2>
+      <ExperienceTimeline />
     </div>
-    )
+  );
 }
