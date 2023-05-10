4e1457191a3a2181443b80a60a206f7d1ba793d9
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-10 08:17:10 +0200

    add react

--- src/components/TechSkills.tsx
+++ src/components/TechSkills.tsx
@@ -10,22 +10,26 @@ export function ProgressCard() {
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
       })}
     >
-      <Text fz="lg" fw={500}>
+      <Text fz="xs" fw={500}>
         HTML
       </Text>
-      <Progress value={74.31} mt="md" size="lg" radius="sm" color="indigo" />
-      <Text fz="lg" fw={500} mt="md">
+      <Progress value={74.31} mb="md" size="lg" radius="sm" color="indigo" />
+      <Text fz="xs" fw={500}>
         CSS
       </Text>
-      <Progress value={74.51} mt="md" size="lg" radius="sm" color="indigo" />
-      <Text fz="lg" fw={500} mt="md">
+      <Progress value={74.51} mb="md" size="lg" radius="sm" color="indigo" />
+      <Text fz="xs" fw={500}>
         JavaScript
       </Text>
-      <Progress value={46.31} mt="md" size="lg" radius="sm" color="indigo" />
-      <Text fz="lg" fw={500} mt="md">
+      <Progress value={50.31} mb="md" size="lg" radius="sm" color="indigo" />
+      <Text fz="xs" fw={500}>
         TypeScript
       </Text>
-      <Progress value={64.31} mt="md" size="lg" radius="sm" color="indigo" />
+      <Progress value={55.31} mb="md" size="lg" radius="sm" color="indigo" />
+      <Text fz="xs" fw={500}>
+        React
+      </Text>
+      <Progress value={55.31} mb="md" size="lg" radius="sm" color="indigo" />
     </Card>
   );
 }
