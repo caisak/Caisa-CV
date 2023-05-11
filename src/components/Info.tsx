fcdf448c755582385c571532bf95c147a6aaed8c
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-11 20:41:51 +0200

    info languages progress bars

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,42 +1,91 @@
-import { Divider, Flex, List, Paper, Text, Title } from "@mantine/core";
+import {
+  Card,
+  createStyles,
+  Divider,
+  Flex,
+  List,
+  Paper,
+  Progress,
+  Text,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  noDecoration: {
+    textDecoration: "none",
+  },
+}));
 
 export function Info() {
+  const { classes, cx } = useStyles();
+  const theme = useMantineTheme();
   return (
     <Paper>
       <div style={{ flex: 3 }}>
-        <Title>Hi! I'm Caisa</Title>
+        <Title mt="xs" mb="xs">
+          Hi! I'm Caisa
+        </Title>
         <Divider />
-        <Text>Front End developer student</Text>
+        <Text mt="xs">Front End developer student</Text>
 
-        <Text>
+        <Text mt="3rem" mb="4rem">
           Text about me bla bla bla what do i want to say no idea but i guess i
           have to say something so eh here goes the sun is sunny and the moon is
           moony
         </Text>
-        <Flex gap="lg">
-        <List size="sm">
-          <List.Item>English - fluent</List.Item>
-          <List.Item>Swedish - native</List.Item>
-          <List.Item>Spanish - light conversational</List.Item>
-        </List>
+        <Card>
+          <Flex direction="column">
+            <Title order={6} mb="xs">
+              Languages
+            </Title>
+            <Text fz="xs" fw={500}>
+              English
+            </Text>
+            <Progress
+              value={80.00}
+              mb="md"
+              size="lg"
+              radius="xs"
+              color="dark.3"
+            />
+            <Text fz="xs" fw={500}>
+              Swedish
+            </Text>
+            <Progress
+              value={100.00}
+              mb="md"
+              size="lg"
+              radius="xs"
+              color="dark.3"
+            />
+            <Text fz="xs" fw={500}>
+              Spanish
+            </Text>
+            <Progress
+              value={35.00}
+              mb="md"
+              size="lg"
+              radius="xs"
+              color="dark.3"
+            />
+          </Flex>
+        </Card>
 
-        <List size="sm">
-          <List.Item>Knitting</List.Item>
-          <List.Item>Reading</List.Item>
-          <List.Item>Stand up paddling</List.Item>
-        </List>
-        </Flex>
       </div>
     </Paper>
   );
 }
 
 export function ProfileImage() {
-    return(
-        <img style={{ 
-            height: '30rem', 
-            objectFit: 'cover' }} 
-            src="./images/caisa.jpg" 
-            alt="profile picture" />
-    )
+  return (
+    <img
+      style={{
+        height: "30rem",
+        objectFit: "cover",
+      }}
+      src="./images/caisa.jpg"
+      alt="profile picture"
+    />
+  );
 }
