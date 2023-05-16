e261c202e671f08da763c7c3bb74df760f9a9cb7
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-05-16 23:56:18 +0200

    add icons to bullets

--- src/components/EducationTimeline.tsx
+++ src/components/EducationTimeline.tsx
@@ -1,29 +1,32 @@
 import { Text, Timeline } from '@mantine/core';
+import { CgCode } from 'react-icons/cg';
+import { FaAnchor, FaPenFancy, FaVideo } from 'react-icons/fa';
+import { MdSportsMartialArts } from 'react-icons/md';
 
 export function EducationTimeline() {
   return (
     <Timeline active={4} bulletSize={24} lineWidth={4} color="teal">
-      <Timeline.Item title="Strömstad Gymnasium">
+      <Timeline.Item bullet={<FaAnchor size={12} />}title="Strömstad Gymnasium">
         <Text size="sm">Upper secondary school</Text>
         <Text size="xs" color="dimmed" mt={4}>2010-2013</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Göteborg Universitet">
+      <Timeline.Item bullet={<MdSportsMartialArts size={12} />}title="Göteborg Universitet">
         <Text size="sm">Sports Coaching</Text>
         <Text size="xs" color="dimmed" mt={4}>2014-2015</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Göteborgs Folkhögskola">
+      <Timeline.Item bullet={<FaVideo size={12} />}title="Göteborgs Folkhögskola">
         <Text size="sm">Film Production</Text>
         <Text size="xs" color="dimmed" mt={4}>2017-2019</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Malmö Folkhögskola">
+      <Timeline.Item bullet={<FaPenFancy size={12} />}title="Malmö Folkhögskola">
         <Text size="sm">Creative writing</Text>
         <Text size="xs" color="dimmed" mt={4}>2019-2020</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Medieinstitutet"  lineVariant="dashed">
+      <Timeline.Item bullet={<CgCode size={12} />}title="Medieinstitutet"  lineVariant="dashed">
         <Text size="sm">Front End Developer</Text>
         <Text size="xs" color="dimmed"  mt={4}>2022-current</Text>
       </Timeline.Item>
--- src/components/ExperienceTimeline.tsx
+++ src/components/ExperienceTimeline.tsx
@@ -1,25 +1,27 @@
 import { Text, Timeline } from "@mantine/core";
+import { FaCoffee, FaRunning, FaCashRegister } from 'react-icons/fa';
+import { ImSpoonKnife } from 'react-icons/im';
 
 export function ExperienceTimeline() {
     return(
         <Timeline active={4} bulletSize={24} lineWidth={4} color="indigo" align="right" >
 
-      <Timeline.Item title="Coop Konsum">
+      <Timeline.Item bullet={<FaCashRegister size={12} />}title="Coop Konsum">
         <Text size="sm">Cashier</Text>
         <Text size="xs" color="dimmed" mt={4}>2011-2012</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Store Support">
+      <Timeline.Item bullet={<FaRunning size={12} />}title="Store Support">
         <Text size="sm">Consultant</Text>
         <Text size="xs" color="dimmed" mt={4}>2016-2019</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Bonum Matmarknad">
+      <Timeline.Item bullet={<ImSpoonKnife size={12} />}title="Bonum Matmarknad">
         <Text size="sm">Deli Clerk</Text>
         <Text size="xs" color="dimmed" mt={4}>2017-2018</Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Espresso House">
+      <Timeline.Item bullet={<FaCoffee size={12} />}title="Espresso House">
         <Text size="sm">Barista</Text>
         <Text size="xs" color="dimmed" mt={4}>2021-current</Text>
       </Timeline.Item>      
